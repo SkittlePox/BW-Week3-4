837d69a3d3bd622d0b1303535dc27663268da3cc
SkittlePox <bspiegel11@gmail.com>
2016-07-26 09:05:08 -0400

    Wrote simplefollow.py

--- scripts/simplefollow.py
+++ scripts/simplefollow.py
@@ -10,15 +10,26 @@ class ObjectDetectorNode:
     def __init__(self):
         self.drive_pub = rospy.Publisher("/vesc/ackermann_cmd_mux/input/navigation", AckermannDriveStamped)
         self.vision = rospy.Subscriber("/scan", LaserScan, self.drive_control)
+        self.right = False
+        self.target = 0.5
+        self.kp = 0.5
         rospy.init_node("slam")
 
     def drive_control(self, msg):
         #Main function
-        drive_command = AckermannDriveStamped(self.header, AckermannDrive(steering_angle=new_steering_angle, speed=2.0))
+        if self.right:
+            ranges = msg.ranges[90:360]
+            flipConstant = -1
+        else:
+            ranges = msg.ranges[990:720]
+            flipConstant = 1
+
+        error = min(ranges) - self.target
+        angle = (error * self.kp * flipConstant)
+
+        drive_command = AckermannDriveStamped(self.header, AckermannDrive(steering_angle=angle, speed=1.0))
         self.drive_pub.publish(drive_command)
 
 if __name__ == "__main__":
-
     node = ObjectDetectorNode()
-
     rospy.spin()
