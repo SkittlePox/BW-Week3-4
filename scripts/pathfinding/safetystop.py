5bca6a2e7cb6831edcbaaa12ba1707c95aacc076
Michelle <mtan123456789@gmail.com>
2016-07-28 15:14:32 +0000

    Added Jon's work

--- scripts/pathfinding/safetystop.py
+++ scripts/pathfinding/safetystop.py
@@ -8,23 +8,15 @@ import numpy
 from ackermann_msgs.msg import AckermannDriveStamped # steering messages
 from sensor_msgs.msg import LaserScan # laser scanner msgs; Joy may be unnecessary
 
-class safetystopnav:
+class safetystop:
 
     def __init__(self):
 
 	rospy.init_node("safetystop")
 	rospy.Subscriber('scan', LaserScan, self.parsescan)
-	self.drivepub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/navigation', AckermannDriveStamped, queue_size=1)	
+	self.drivepub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/safety', AckermannDriveStamped, queue_size=1)	
 
-    def drive(self, angle, speed):
-
-        ackmsg = AckermannDriveStamped()
-       	ackmsg.drive.speed = speed
-        ackmsg.drive.steering_angle = angle
-
-        self.drivepub.publish(ackmsg)
-
-   def parsescan(self, scan):
+    def parsescan(self, scan):
 
 	if min(scan.ranges[0:1080])<.3:
 	    self.drive(0,0)
@@ -32,5 +24,5 @@ class safetystopnav:
 
 if __name__ == '__main__':
 
-    node = Fieldnav()
+    node = safetystop()
     rospy.spin()
