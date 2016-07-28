d4f1584156cc1adfe56398969ed7c9a08c6bf4e5
SkittlePox <bspiegel11@gmail.com>
2016-07-28 09:21:57 -0400

    Added face detection code

--- scripts/img/recognizer.py
+++ scripts/img/recognizer.py
@@ -33,7 +33,44 @@ class Recog:
         image_cv = self.bridge.imgmsg_to_cv2(image_msg)
 
         # Image processing starts here
+        color_contour, color_scheme, display_text = self.color_search(image_cv)
+        faces = self.face_search(image_cv)
 
+        if (len(faces) > 0):
+            # Drawing rectangle for face
+            cv2.rectangle(image, (faces[0].x, faces[0].y), (faces[0].x + faces[0].w, faces[0].y + faces[0].h), (0, 255, 0), 2)
+            display_text = "face"
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(image_cv, display_text, (faces[0].x + faces[0].x / 2, faces[0].y + 3 * faces[0].h / 4), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        else:
+            # Drawing contours
+            cv2.drawContours(image_cv, [the_one], -1, (color_scheme.b, color_scheme.g, color_scheme.r))
+            x, y, w, h = cv2.boundingRect(the_one)
+            cv2.rectangle(image_cv, (x, y), (x + w, y + h), (color_scheme.b, color_scheme.g, color_scheme.r, 2))
+            cv2.circle(image_cv, (x + w / 2, y + h / 2), 4, (255, 255, 255), -1)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(image_cv, display_text, (x + x / 2, y + 3 * h / 4), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+        # Image processing stops here
+
+        self.pub_found.publish("Found",display_text)
+        self.pub_image.publish(self.bridge.cv2_to_imgmsg(image_cv, display_text))
+        #cv2.imwrite("~/racecar/challenge_photos/%s.png"%(display_text), image_cv)
+
+        self.thread_lock.release()
+
+    def face_search(self, image_cv):
+        faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        gray = cv2.cvtColor(image_cv, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        return faces
+
+    def color_search(self, image_cv):
         image_hsv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2HSV)
 
         filters_red = [np.array([0, 165, 100]), np.array([6, 255, 255])]  # Red
@@ -103,21 +140,7 @@ class Recog:
                 color_scheme = color_objects[i]
                 self.the_time = time.clock()
 
-        # Drawing
-        cv2.drawContours(image_cv, [the_one], -1, (color_scheme.b, color_scheme.g, color_scheme.r))
-        x, y, w, h = cv2.boundingRect(the_one)
-        cv2.rectangle(image_cv, (x, y), (x + w, y + h), (color_scheme.b, color_scheme.g, color_scheme.r, 2))
-        cv2.circle(image_cv, (x + w / 2, y + h / 2), 4, (255, 255, 255), -1)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(image_cv, display_text, (x + x / 2, y + 3 * h / 4), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
-        # Image processing stops here
-
-        self.pub_found.publish("Found",display_text)
-        self.pub_image.publish(self.bridge.cv2_to_imgmsg(image_cv, display_text))
-        #cv2.imwrite("~/racecar/challenge_photos/%s.png"%(display_text), image_cv)
-
-        self.thread_lock.release()
-
+        return the_one, color_scheme, display_text
 
 if __name__ == "__main__":
     rospy.init_node('Recog')
