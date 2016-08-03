022bbecded55a19e18f47748d22c877484d28ba8
Max Krieger <themaxaxis@gmail.com>
2016-08-03 10:29:21 -0400

    fix

--- scripts/ptprix/controller.py
+++ scripts/ptprix/controller.py
@@ -32,14 +32,14 @@ class Controller:
         scan_x_unit_vectors = -np.cos(scan_rad_angles)
         scan_y_unit_vectors = -np.sin(scan_rad_angles)
 
-        scan_x_components = (self.charge_laser_particle * scan_x_unit_vectors)
-        / np.square(msg.ranges)
-        scan_y_components = (self.charge_laser_particle * scan_y_unit_vectors)
-        / np.square(msg.ranges)
+        scan_x_components = (self.charge_laser_particle *
+                             scan_x_unit_vectors) / np.square(msg.ranges)
+        scan_y_components = (self.charge_laser_particle *
+                             scan_y_unit_vectors) / np.square(msg.ranges)
 
         # Add the potential for the point behind the robot (to give it a kick)
-        kick_x_component = np.ones(1) * self.charge_forward_boost
-        / self.boost_distance**2.0
+        kick_x_component = np.ones(1) *
+        self.charge_forward_boost / self.boost_distance**2.0
         kick_y_component = np.zeros(1)
 
         total_x_component = np.sum(scan_x_components) +
