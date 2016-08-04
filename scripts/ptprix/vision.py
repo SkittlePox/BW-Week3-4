f2ae53d4627cd4839ef57b7b887e803c97c1f7c5
Max Krieger <themaxaxis@gmail.com>
2016-08-04 10:18:59 -0400

    fixed

--- scripts/ptprix/vision.py
+++ scripts/ptprix/vision.py
@@ -25,26 +25,46 @@ class Vision:
         thread.start()
 
     def processImage(self, image_msg):
-        noneFound = False
         if not self.thread_lock.acquire(False):
             return
+        ms = self.findPoly(image_msg, 1)
+        if(ms is None):
+            ms = self.findPoly(image_msg, 0)
+        if(ms is None):
+            ms = self.getDefaultMsg()
 
-        ms = findPoly(image_msg, np.array([54, 30, 60]),
-                      np.array([72, 255, 255]))
-
+        self.pub.publish(ms)
         self.thread_lock.release()
 
-    def findPoly(self, mn, mx):
+    def getDefaultMsg(self):
+        msg = Int32MultiArray()
+        msg.data = [0, 0, -1]
+        return msg
+
+    def findPoly(self, image_msg, mn):
         img = self.bridge.imgmsg_to_cv2(image_msg)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, mn, mx)
+        if(mn == 1):
+            greens = [np.array([54, 110, 10]), np.array([72, 180, 200])]
+            mask = cv2.inRange(hsv, greens[0], greens[1])
+        else:
+            filters_red = [np.array([0, 165, 70]), np.array([6, 255, 255])]
+            filters_red2 = [np.array([170, 165, 140]),
+                            np.array([180, 255, 255])]
+            mask_red = cv2.inRange(image_hsv, filters_red[0], filters_red[1])
+            mask_red2 = cv2.inRange(image_hsv, filters_red2[0],
+                                    filters_red2[1])
+            mask = cv2.bitwise_or(mask_red, mask_red2)
+
         mask = cv2.erode(mask, (3, 3), iterations=1)
         contours, h = cv2.findContours(mask,
                                        cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cv2.drawContours(img, contours, -1, (0, 0, 255), 2)
         sorted_contours = sorted(contours, key=lambda c: cv2.contourArea(c),
                                  reverse=True)
-        if len(sorted_contours < 1):
+        self.pub_image.publish(
+                self.bridge.cv2_to_imgmsg(img, "bgr8"))
+        if len(sorted_contours) < 1:
             return None
 
         c = sorted_contours[0]
@@ -52,27 +72,21 @@ class Vision:
 
         perim = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.05 * perim, True)
-
         if len(approx) != 4:
             return None
-
         cv2.drawContours(img, [c], -1, (255, 0, 0), 3)
         cv2.drawContours(img, [approx], -1, (0, 255, 0), 5)
 
         M = cv2.moments(approx)
+        if(M['m00'] == 0 or M['m01'] == 0):
+            return None
         x, y = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
 
         msg = Int32MultiArray()
-        msg.data = [x, area, color]  # area or height?
-        try:
-            self.pub_image.publish(
-                self.bridge.cv2_to_imgmsg(img, "bgr8"))
-        except CvBridgeError as e:
-            print(e)
-
+        msg.data = [x, area, mn]
         return msg
 
 if __name__=="__main__":
     rospy.init_node('Vision')
-    colortracker = ColorTracker()
+    vision = Vision()
     rospy.spin()
