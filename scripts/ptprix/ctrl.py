597c7612b5433841a15e97ba7f284132c9a89ca4
Max Krieger <maxkrieger@users.noreply.github.com>
2016-08-04 18:05:59 -0400

    Update ctrl.py

--- scripts/ptprix/ctrl.py
+++ scripts/ptprix/ctrl.py
@@ -35,7 +35,7 @@ class Controller:
         coeff = 300
         print("ye")
         print(x, area, color)
-        if area > 800 and color != -1:
+        if area > 700 and color != -1:
             if color == 0:
                 self.y_components['leftCharge'] = -coeff
                 print("right")
